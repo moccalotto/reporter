0b725b399a8dc744cca544383dd1ebfe5e70a75f
Kim Ravn Hansen <moccalotto@gmail.com>
2015-12-18 09:05:34 +0100

    Commentary

--- src/Ensure.php
+++ src/Ensure.php
@@ -4,22 +4,33 @@ namespace Moccalotto\Reporter;
 
 use Moccalotto\Reporter\Exceptions\EnsureException;
 
+/**
+ * Class for handling assertions.
+ */
 class Ensure
 {
-    protected static function terminate($message, $code)
-    {
-        echo $message;
-        flush();
-        die((int) $code);
-    }
-
-    public static function that($condition, $message, $exitCode = 1)
+    /**
+     * Ensure that a given condition is true.
+     *
+     * @param bool $condition
+     * @param string $message
+     *
+     * @throws EnsureException
+     */
+    public static function that($condition, $message)
     {
         if (!$condition) {
-            throw new EnsureException($message, $exitCode);
+            throw new EnsureException($message);
         }
     }
 
+    /**
+     * Ensure that a given file exists (and is indeed a file).
+     *
+     * @param string $file  The complete path of the file
+     *
+     * @throws EnsureException
+     */
     public static function fileExists($file)
     {
         static::that(file_exists($file), sprintf(
@@ -33,6 +44,13 @@ class Ensure
         ));
     }
 
+    /**
+     * Ensure that a file is readable.
+     *
+     * @param string $file  The complete path of the file
+     *
+     * @throws EnsureException
+     */
     public static function fileIsReadable($file)
     {
         static::fileExists($file);
@@ -43,21 +61,50 @@ class Ensure
         ));
     }
 
+    /**
+     * Ensure that a variable is a resource
+     *
+     * @param mixed $data
+     *
+     * @throws EnsureException
+     */
     public static function isResource($data, $error_message = 'Data must be a resource')
     {
         static::that(is_resource($data), $error_message);
     }
 
+    /**
+     * Ensure that a variable is an array
+     *
+     * @param mixed $data
+     *
+     * @throws EnsureException
+     */
     public static function isArray($data, $error_message = 'Data must be an array')
     {
         static::that(is_array($data), $error_message);
     }
 
+    /**
+     * Ensure that a variable is an object
+     *
+     * @param mixed $data
+     *
+     * @throws EnsureException
+     */
     public static function isObject($data, $error_message = 'Data must be an object')
     {
         static::that(is_object($data), $error_message);
     }
 
+    /**
+     * Ensure that a string contains valid json
+     *
+     * @param string $jsonString
+     * @param string $error_message
+     *
+     * @throws EnsureException
+     */
     public static function validJson($jsonString, $error_message = null)
     {
         $data = json_decode($jsonString, true);
@@ -68,17 +115,29 @@ class Ensure
         ));
     }
 
+    /**
+     * Ensure that two variables are equal (==)
+     *
+     * @param mixed $var1
+     * @param mixed $var1
+     * @param mixed $message
+     *
+     * @throws EnsureException
+     */
     public static function isEqual($var1, $var2, $message = 'Unexpected value')
     {
         static::that($var1 == $var2, $message);
     }
 
+    /**
+     * @param string $pattern
+     * @param string $string
+     * @param string|null $message
+     *
+     * @throws EnsureException
+     */
     public static function matches($pattern, $string, $message = null)
     {
-        if (preg_match($pattern, $string)) {
-            return;
-        }
-
         if (null === $message) {
             $message = sprintf(
                 'The string "%s" does not adhere to the pattern "%s"',
@@ -87,6 +146,6 @@ class Ensure
             );
         }
 
-        static::terminate($message);
+        static::that(preg_match($pattern, $string), $message);
     }
 }
