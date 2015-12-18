fe733ef055154ded7865d63816607d6e56fd742c
Kim Ravn Hansen <moccalotto@gmail.com>
2015-12-18 09:01:20 +0100

    Commentary

--- src/Signer.php
+++ src/Signer.php
@@ -4,15 +4,37 @@ namespace Moccalotto\Reporter;
 
 class Signer
 {
+    /**
+     * HMAC key
+     *
+     * @var string
+     */
     protected $key;
+
+    /**
+     * HMAC algorith
+     *
+     * @var string
+     */
     protected $algorithm;
 
+    /**
+     * Constructor.
+     *
+     * @param string $algorithm
+     * @param string $key
+     */
     public function __construct($algorithm, $key)
     {
         $this->algorithm = $algorithm;
         $this->key = $key;
     }
 
+    /**
+     * Create a signature for a given string.
+     *
+     * @param string $data
+     */
     public function signature($data)
     {
         return hash_hmac($this->algorithm, $data, $this->key);
